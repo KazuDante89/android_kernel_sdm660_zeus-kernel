e4857066c1ff5f7f14a0028d4c6d0119713f8e46
kazudante89 <karlemmanueldorvil@gmail.com>
2020-07-01 13:18:56 -0400

    change again 	modified:   compile.sh

--- compile.sh
+++ compile.sh
@@ -24,7 +24,7 @@ export KBUILD_BUILD_USER=KazuDante
 # Export Machine name
 export KBUILD_BUILD_HOST=xdadevelopers
 # Compiler String
-CC=/media/system/root1/android_prebuilts_clang_host_linux-x86_clang-6364210/bin/clang-10
+CC=/media/system/root1/aosp-clang/clang-10
 export KBUILD_COMPILER_STRING="$(${CC} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
 # Make and Clean
 make O=$out clean
@@ -34,9 +34,9 @@ make O=$out ARCH=arm64 aurora_defconfig
 DATE_START=$(date +"%s")
 # Build Kernel
 make O=$out ARCH=arm64 \
-CC="/media/system/root1/android_prebuilts_clang_host_linux-x86_clang-6364210/bin/clang-10" \
-CLANG_TRIPLE=aarch64-linux-gnu- \
-CROSS_COMPILE="/media/system/root1/aarch64-linux-gnu/aarch64-linux-gnu-" \
+CC="/media/system/root1/aosp-clang/clang-10" \
+CLANG_TRIPLE=aarch64-maestro-linux-gnu- \
+CROSS_COMPILE="/media/system/root1/aarch64-maestro-linux-android/aarch64-maestro-linux-gnu-" \
 CROSS_COMPILE_ARM32="/media/system/root1/arm-maestro-linux-gnueabi/bin/arm-maestro-linux-gnueabi-" \
 -j$(nproc --all) Image.gz-dtb
 DATE_END=$(date +"%s")
