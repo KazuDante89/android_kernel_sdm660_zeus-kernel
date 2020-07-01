060a4c626bef62abee1782a5ca3d68f2babdd00e
kazudante89 <karlemmanueldorvil@gmail.com>
2020-07-01 13:22:25 -0400

    fix again 	modified:   compile.sh

--- compile.sh
+++ compile.sh
@@ -24,7 +24,7 @@ export KBUILD_BUILD_USER=KazuDante
 # Export Machine name
 export KBUILD_BUILD_HOST=xdadevelopers
 # Compiler String
-CC=/media/system/root1/aosp-clang/clang
+CC=/media/system/root1/aosp-clang/bin/clang-10
 export KBUILD_COMPILER_STRING="$(${CC} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
 # Make and Clean
 make O=$out clean
@@ -34,7 +34,7 @@ make O=$out ARCH=arm64 aurora_defconfig
 DATE_START=$(date +"%s")
 # Build Kernel
 make O=$out ARCH=arm64 \
-CC="/media/system/root1/aosp-clang/clang" \
+CC="/media/system/root1/aosp-clang/bin/clang-10" \
 CLANG_TRIPLE=aarch64-maestro-linux-gnu- \
 CROSS_COMPILE="/media/system/root1/aarch64-maestro-linux-android/aarch64-maestro-linux-gnu-" \
 CROSS_COMPILE_ARM32="/media/system/root1/arm-maestro-linux-gnueabi/bin/arm-maestro-linux-gnueabi-" \
